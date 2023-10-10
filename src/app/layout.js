998e2fe7338c134e1fec48c5d5d37f8964c70016
CARTOON01 <brianngugi224@gmail.com>
2023-10-11 00:15:52 +0300

    NavBar and Footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,12 +6,16 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Blog App',
   description: 'The best blog app!',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="container">
+          <div className="wrapper">{children}</div>
+            </div>
+      </body>
     </html>
-  )
+  );
 }
